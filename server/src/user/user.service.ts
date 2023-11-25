fe6242ff94b9884f18baa16257d7a219fb5b0756
hogisim314 <hogisim314@gmail.com>
2023-11-25 10:32:33 +0900

    checkaccount 버그 수정

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
